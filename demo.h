a644112663ad7ec064cd84835d4f0519f8726418
tradesend <diego.moran-jurado@epitech.eu>
2015-09-30 15:13:35 +0200

    apres suivi 1, notes rapides

--- demo.h
+++ demo.h
@@ -5,7 +5,7 @@
 ** Login   <moran-_d@epitech.net>
 ** 
 ** Started on  Wed Sep 30 13:12:16 2015 moran-_d
-** Last update Wed Sep 30 14:06:54 2015 moran-_d
+** Last update Wed Sep 30 15:05:08 2015 moran-_d
 */
 
 /*
@@ -35,7 +35,7 @@ struct ___struct__Geometry__Carre
 ___Geometry__Rectangle* ___Geometry__Rectangle__Function__ctor__int__int__int__int___Rectangle__Pointer(int, int, int, int);
 ___Geometry__Rectangle* ___Geometry__Rectangle__Function__ctor_____Geometry__Point__Pointer__int__int___RectanglePtr(___Geometry__Point *, int, int);
 void ___Geometry__Rectangle__Function__dtor___void();
-___Geometry__Point* ___Geometry__Rectangle__Function__findMiddle______Geometry__Point__Pointer();
+___Geometry__Point* ___Geometry__Rectangle__Function__findMiddle______Geometry__Point__Pointer(___Geometry__Rectangle*);
 
 /* ___Geometry__Carre */
 ___Geometry__Carre* ___Geometry__Carre__Function__ctor__int__int__int______Geometry__Carre__Pointer(int, int, int);
